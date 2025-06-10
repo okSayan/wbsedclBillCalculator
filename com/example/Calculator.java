2ae12dbefeab467a8455d0f8cf01a376abe4043d
Sayan <thesayanbar@gmail.com>
2025-06-11 00:36:33 +0530

    Update Calculator.java

--- com/example/Calculator.java
+++ com/example/Calculator.java
@@ -15,6 +15,7 @@ public class Calculator {
         myWatt = loadInput("input.txt");
         List<double[]> slabRates = loadSlabRates("slabs.txt");
         double billAmount = calculateBill(myWatt, slabRates);
+        System.out.println("\n\nYour: KWh: "+myWatt);
         System.out.println("\n\nYour estimated (Energy Charge) is: \t" + billAmount);
         System.out.println("Actual bill is subject to other factors like (Meter Charge, Govt. Subsidies, Adjustments, etc.) This program is just to see if the (Energy Charge) is correct or not.");
     }
